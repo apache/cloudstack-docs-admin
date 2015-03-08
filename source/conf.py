c8c282f3e1cc1849c381d1164ebbc5a216507628
Pierre-Luc Dion <pdion891@apache.org>
2015-03-08 11:02:10 -0400

    dummy commit to close #1, file not in this repo anymore.

--- source/conf.py
+++ source/conf.py
@@ -101,7 +101,6 @@ pygments_style = 'sphinx'
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
