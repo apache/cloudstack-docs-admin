f92fad262aea65c9dc8c84ee65b04e9f7c5f94be
Antoine Le Morvan <antoine@le-morvan.com>
2016-08-20 00:03:28 +0200

    50% des chaines sont traduites en FR

--- make.sh
+++ make.sh
@@ -1,3 +1,33 @@
-rm -fr build
-mkdir build
-sphinx-build -b html source build
+#!/bin/bash
+HOST='ftp.online.net'
+USER='cloudstack@le-morvan.com'
+PASSWORD='St@ckCloud!01'
+
+FILE=/home/antoine/Git/cloudstack-docs-admin/build/html
+
+rm -fr $FILE
+mkdir $FILE
+
+echo "Generation du doc en HTML"
+sphinx-build -b html -d build/doctrees  -D language='fr' source build/html
+
+echo "Generation du PDF"
+make latexpdf
+
+echo "Generation du latex"
+make latex
+
+pandoc -s build/latex/CloudStackAdministrationDocumentation.tex -o build/CloudStackAdministrationDocumentation.rtf
+
+#echo "Copie en FTP"
+#ftp -n $HOST << END_SCRIPT
+#quote USER $USER
+#quote PASS $PASSWORD
+#lcd $FILE
+#mput *
+#quit
+#END_SCRIPT
+
+
+
+
